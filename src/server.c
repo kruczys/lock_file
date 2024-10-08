5bf80c8bd551e02ea1d1f37da1e6bc82dc25149c
Konrad Kreczko <konradkreczko@gmail.com>
2024-10-08 23:19:17 +0200

    feat: Added commented algorithm for server

--- src/server.c
+++ src/server.c
@@ -2,3 +2,11 @@ int main()
 {
     return 0;
 }
+
+// 1. Czekaj az plik bedzie wolny
+// 2. zajmij plik
+// 3. przeczytaj ostatnia linie
+// 4. wykonaj obliczenia
+// 5. dopisz wynik do pliku
+// 6. zwolnij plik
+// 7. wroc do 1 dopoki uzytkownik nie wyjdzie
