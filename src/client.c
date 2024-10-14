6b1abdacb6e7b20ced485191e0c03dfa891dc856
Konrad Kreczko <konradkreczko@gmail.com>
2024-10-14 16:58:00 +0200

    chore: changed gitignore

--- src/client.c
+++ src/client.c
@@ -4,24 +4,24 @@
 #include <unistd.h>
 
 void write_to_file() {
-  return;
+    return;
 }
 
 int read_user_input() {
-  int number;
-  scanf("%d", &number);
-  return number;
+    int number;
+    scanf("%d", &number);
+    return number;
 }
 
 int main() {
-  int fd = open("buffer", O_RDWR|O_APPEND|O_CREAT, 0666);
-  int input = read_user_input();
+    int fd = open("buffer", O_RDWR|O_APPEND|O_CREAT, 0666);
+    int input = read_user_input();
 
-  write(fd, &input, sizeof(input));
-  write(fd, "\n", 1);
+    write(fd, &input, sizeof(input));
+    write(fd, "\n", 1);
 
-  close(fd);
-  exit(0);
+    close(fd);
+    exit(0);
 }
 
 // 1. Sprawdz czy plik istnieje, jesli nie to utworz, jesli tak to nadpisz
